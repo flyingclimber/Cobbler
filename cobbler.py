13162672940b905f78a495051ded3b787cd3ac3b
Tomasz Finc <tomasz@gmail.com>
2016-03-22 15:50:02 -0700

    Moving patching work to ips.py, adding DMG-NDJ specific layouts, updating cobbler to put it all together

--- cobbler.py
+++ cobbler.py
@@ -1,90 +1,86 @@
 #!/usr/bin/env python
 
-"""
-    cobbler.py - IPS patch creator
-"""
+import ips
+import csv
+import json
 
-PATCH = 'test.ips'
-
-
-class Patch:
-    """
-        High level view of a patch
-    """
-    def __init__(self):
-        pass
-
-    def create_patch(self):
-        pass
-
-
-class Ips(Patch):
-    """
-        Patch conforming to http://zerosoft.zophar.net/ips.php
-    """
-    def __init__(self):
-        self.type = self.__class__
-        self.header = 'PATCH'
-        self.footer = 'EOF'
-        self.hunks = []
-
-    def add_hunk(self, hunk):
-        """
-            Add a new block to the total patchset
-        """
-        if hunk.offset:
-            self.hunks.append(hunk)
-        else:
-            print "Skipping: No offset given"
-
-    def create_patch(self):
-        """
-            Output a final patch to disk
-        """
-        with open(PATCH, 'wb') as f:
-            f.write(self.header)
-
-            for hunk in self.hunks:
-                f.write(hunk.offset)
-                f.write(hunk.size)
-                f.write(hunk.data)
-
-            f.write(self.footer)
-
-
-class Hunk:
-    """
-        Individual IPS record
-    """
-    def __init__(self, offset, size, data):
-        self.offset = offset
-        self.size = size
-        self.data = data
+CSV = 'Nekketsu.csv'
+TILE_LAYOUT_JSON = 'tile_layout.json'
+ROM_LAYOUT_JSON = 'rom_layout.json'
 
 
 class Cobbler:
     """
         Someone to put it all together
     """
-    def __init__(self):
-        pass
+    def __init__(self, csv):
+        self.csv = csv
+        self.ips = ips.Ips()
+
+    def parse_csv(self):
+        with open(self.csv, 'rb') as g:
+            reader = csv.DictReader(g)
+
+            for row in reader:
+                if row['Start']:
+                    update = Update(row['Start'], row['End'], row['Edited'])
+                    print update
+                    print update.convert_to_tile()
+
+
+class Update:
+    def __init__(self, start, end, data):
+        self.start = start
+        self.end = end
+        self.data = data
+
+    def __str__(self):
+        return '{}-{} {}'.format(self.start, self.end, self.data)
+
+    def convert_to_tile(self):
+        tile_layout = TileLayout('DMG-NDJ')
+        rom_layout = RomLayout('DMG-NDJ')
+        tile_set = rom_layout.get_tile_set('DMG-NDJ', self.start)
+        res = bytearray()
+
+        for char in self.data:
+            res.append(tile_layout.get_hex(char, tile_set))
+
+        return res
+
+
+class Rom:
+    def __init__(self, serial):
+        self.serial = serial
+        self.rom_layout = RomLayout(serial)
+        self.tile_layout = TileLayout(serial)
+
+
+class RomLayout:
+    def __init__(self, serial):
+        with open(ROM_LAYOUT_JSON, 'r') as g:
+            self.mapping = json.load(g)
+
+    def get_tile_set(self, serial, address):
+        for rom_range in self.mapping[serial]:
+            if int(self.mapping[serial][rom_range]['start'], 16) <= int(address, 16) <= int(self.mapping[serial][rom_range]['end'], 16):
+                return self.mapping[serial][rom_range]['tile_set']
+
+
+class TileLayout:
+    def __init__(self, serial):
+        self.serial = serial
+
+        with open(TILE_LAYOUT_JSON, 'r') as g:
+            self.mapping = json.load(g)
+
+    def get_hex(self, char, tile_set):
+        return self.mapping[self.serial][tile_set][char.upper()]
 
 
 def main():
-    """
-        Main runner
-    """
-    patch = Ips()
-    h1_ = Hunk(bytearray([10]),
-               bytearray([2]),
-               bytearray([50, 100, 104]))
-    h2_ = Hunk(bytearray([20]),
-               bytearray([50]),
-               bytearray([10, 20, 30, 40, 50, 255]))
-    patch.add_hunk(h1_)
-    patch.add_hunk(h2_)
-    patch.create_patch()
-
+    cobbler = Cobbler(CSV)
+    cobbler.parse_csv()
 
 if __name__ == "__main__":
     main()
---
+++ ips.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+"""
+    ips.py - IPS patch creator
+"""
+
+PATCH = 'test.ips'
+
+
+class Patch:
+    """
+        High level view of a patch
+    """
+    def __init__(self):
+        pass
+
+    def create_patch(self):
+        pass
+
+
+class Ips(Patch):
+    """
+        Patch conforming to http://zerosoft.zophar.net/ips.php
+    """
+    def __init__(self):
+        self.type = self.__class__
+        self.header = 'PATCH'
+        self.footer = 'EOF'
+        self.hunks = []
+
+    def add_hunk(self, hunk):
+        """
+            Add a new block to the total patchset
+        """
+        if hunk.offset:
+            self.hunks.append(hunk)
+        else:
+            print "Skipping: No offset given"
+
+    def create_patch(self):
+        """
+            Output a final patch to disk
+        """
+        with open(PATCH, 'wb') as f:
+            f.write(self.header)
+
+            for hunk in self.hunks:
+                f.write(hunk.offset)
+                f.write(hunk.size)
+                f.write(hunk.data)
+
+            f.write(self.footer)
+
+
+class Hunk:
+    """
+        Individual IPS record
+    """
+    def __init__(self, offset, size, data):
+        self.offset = offset
+        self.size = size
+        self.data = data
+
+
+def main():
+    """
+        Main runner
+    """
+    patch = Ips()
+    h1_ = Hunk(bytearray([10]),
+               bytearray([2]),
+               bytearray([50, 50, 100, 104]))
+    h2_ = Hunk(bytearray([20]),
+               bytearray([50]),
+               bytearray([10, 10, 20, 30, 40, 50, 255]))
+    patch.add_hunk(h1_)
+    patch.add_hunk(h2_)
+    patch.create_patch()
+
+
+if __name__ == "__main__":
+    main()
