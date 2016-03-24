50c401039875b75834a3ff44eec2e126f9ca1484
Tomasz Finc <tomasz@gmail.com>
2016-03-24 09:05:23 -0700

    Moving rom class and functions to rom.py

---
+++ rom.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import json
+
+"""
+    rom.py - Rom related functionality
+"""
+
+DATA_DIR = 'data'
+TILE_LAYOUT_JSON = 'tile_layout.json'
+ROM_LAYOUT_JSON = 'rom_layout.json'
+
+
+class Rom:
+    """
+        Game we are patching
+    """
+    def __init__(self, serial):
+        self.serial = serial
+        self.rom_layout = RomLayout(serial)
+        self.tile_layout = TileLayout(serial)
+
+    def __str__(self):
+        return self.serial
+
+
+class RomLayout:
+    """
+        Layout table that maps rom address to a tile set
+    """
+    def __init__(self, serial):
+        self.serial = serial
+
+        with open(DATA_DIR + "/" + ROM_LAYOUT_JSON, 'r') as layout_file:
+            self.mapping = json.load(layout_file)
+
+    def get_tile_set(self, serial, address):
+        """
+            Return the tile set that maps to the given address
+        """
+        for rom_range in self.mapping[serial]:
+            if int(self.mapping[serial][rom_range]['start'], 16) <= \
+                    int(address, 16) <= \
+                    int(self.mapping[serial][rom_range]['end'], 16):
+                return self.mapping[serial][rom_range]['tile_set']
+
+
+class TileLayout:
+    """
+        Layout table that maps tiles to decimal values
+    """
+    def __init__(self, serial):
+        self.serial = serial
+
+        with open(DATA_DIR + "/" + TILE_LAYOUT_JSON, 'r') as layout_file:
+            self.mapping = json.load(layout_file)
+
+    def get_hex(self, char, tile_set):
+        """
+            Return the decimal tile mapping for a character using a tile
+        """
+        return self.mapping[self.serial][tile_set][char.upper()]
